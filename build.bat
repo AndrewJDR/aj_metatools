d783b883020751ae290b38b9045861d747fe633f
Andrew Johnson <ajohnson@draster.com>
2016-09-26 19:46:40 -0700

    Windows build fix.

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
-REM This is untested
 @echo off
 
+md _build
 SET OPTS=/W4 /wd4310 /wd4100 /wd4201 /wd4505 /wd4996 /wd4127 /wd4510 /wd4512 /wd4610 /wd4390 /WX
 SET OPTS=/GR- /EHa- /nologo /FC
 
-cl %OPTS% cpp_print_all_ident_vals.cpp /Zi /Fecpp_print_all_ident_vals
+cl %OPTS% cpp_print_all_ident_vals.cpp /Zi /Fe_build\cpp_print_all_ident_vals
